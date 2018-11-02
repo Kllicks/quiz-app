038c6fa18104a1703bb56640db36d2eca669d461
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-02 10:14:28 -0400

    removes comments from seed

--- seed.sql
+++ seed.sql
@@ -18,3 +18,35 @@ values
     ('quiz #2'),
     ('quiz #3')
 ;
+
+insert into questions
+    (question_text, correct_answer, quiz_id)
+values
+    ('why?', 'because', 1), 
+    ('when?', 'now', 1),   
+    ('how?', 'i don''t care', 1),  
+
+    ('who?', 'oakley', 2),
+    ('what?', 'mew', 2),
+    ('woof?', 'bark', 2),
+
+    ('tabs or spaces?', 'spaes', 3),    
+    ('python or javascript?', 'javascript', 3),
+    ('sequel or s-q-l?', 'sequel', 3)   
+;
+
+insert into answers
+    (answer_text, question_id, student_id)
+values
+    ('because', 1, 1),
+    ('later', 2, 1),
+    ('with my fist', 3, 1),
+    
+    ('why not', 1, 3),
+    ('now', 2, 3),
+    ('however, man...', 3, 3),
+    
+    ('because', 1, 5),
+    ('now', 2, 5),
+    ('aliens', 3, 5)
+;
